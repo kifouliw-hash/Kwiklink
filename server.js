4cff41fe6ad2252d0a67fe5b1f6e3cb7be61cede
kifouliw-hash <kifouliw@gmail.com>
2025-10-22 15:04:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// ✅ Rendre le dossier "publique" accessible (corrigé)
-app.use(express.static(path.join(__dirname, "publique")));
+// ✅ Rendre le dossier "public" accessible (corrigé)
+app.use(express.static(path.join(__dirname, "public")));
 
 // ✅ Route principale
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "publique", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // ✅ Port
